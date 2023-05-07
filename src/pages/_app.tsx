5ed6cbf12a2f6c081513bb4162d27176041e303a
Marcin Mosiejko <marcin@stratokit.io>
2023-05-07 12:06:09 +0200

    posting & loading states + toast

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,16 +1,18 @@
-import { type AppType } from "next/app";
-import { ClerkProvider } from "@clerk/nextjs";
+import { type AppType } from 'next/app'
+import { ClerkProvider } from '@clerk/nextjs'
+import { Toaster } from 'react-hot-toast'
 
-import { api } from "~/utils/api";
+import { api } from '~/utils/api'
 
-import "~/styles/globals.css";
+import '~/styles/globals.css'
 
 const MyApp: AppType = ({ Component, pageProps }) => {
   return (
     <ClerkProvider {...pageProps}>
+      <Toaster position="bottom-center" />
       <Component {...pageProps} />
     </ClerkProvider>
-  );
-};
+  )
+}
 
-export default api.withTRPC(MyApp);
+export default api.withTRPC(MyApp)
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,8 +9,9 @@ import relativeTime from 'dayjs/plugin/relativeTime'
 
 import { type NextPage } from 'next'
 import { type RouterOutputs } from '~/utils/api'
-import { LoadingPage } from '~/components/loading'
+import { LoadingPage, LoadingSpinner } from '~/components/loading'
 import { useState } from 'react'
+import toast from 'react-hot-toast'
 
 dayjs.extend(relativeTime)
 
@@ -25,6 +26,14 @@ const CreatePostWizard = () => {
     onSuccess: () => {
       setInput('')
       void ctx.posts.getAll.invalidate() // Invalidate the cache to refetch the posts
+    },
+    onError: (e) => {
+      const errorMessage = e.data?.zodError?.fieldErrors.content
+      if (errorMessage && errorMessage[0]) {
+        toast.error(errorMessage[0])
+      } else {
+        toast.error('Failed to post! Please try again later.')
+      }
     },
   })
 
@@ -45,9 +54,22 @@ const CreatePostWizard = () => {
         type="text"
         value={input}
         onChange={(e) => setInput(e.target.value)}
+        onKeyDown={(e) => {
+          if (e.key === 'Enter') {
+            e.preventDefault()
+            if (input !== '') {
+              mutate({ content: input })
+            }
+          }
+        }}
         disabled={isPosting}
       />
-      <button onClick={() => mutate({ content: input })}>Post</button>
+      <div className="flex justify-center items-center">
+        {input !== '' && !isPosting && (
+          <button onClick={() => mutate({ content: input })}>Post</button>
+        )}
+        {isPosting && <LoadingSpinner size={20} />}
+      </div>
     </div>
   )
 }
