c8d5a29c38cdca5248cd6efdba4d71cb6da9ee78
Marcin Mosiejko <marcin@stratokit.io>
2023-05-07 18:29:07 +0200

    page layout + metadata

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import { type AppType } from 'next/app'
 import { ClerkProvider } from '@clerk/nextjs'
 import { Toaster } from 'react-hot-toast'
+import Head from 'next/head'
 
 import { api } from '~/utils/api'
 
@@ -8,7 +9,16 @@ import '~/styles/globals.css'
 
 const MyApp: AppType = ({ Component, pageProps }) => {
   return (
+    // head will be rendered on every page but overrideable by individual pages
     <ClerkProvider {...pageProps}>
+      <Head>
+        <title>Chirp</title>
+        <meta
+          name="description"
+          content="emoji twitter alternative done with t3-stack-tutorial"
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <Toaster position="bottom-center" />
       <Component {...pageProps} />
     </ClerkProvider>
