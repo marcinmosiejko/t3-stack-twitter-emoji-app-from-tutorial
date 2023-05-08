a1c2681c1a776f1a5de5d85d7b87533541368231
Marcin Mosiejko <marcin@stratokit.io>
2023-05-08 14:46:04 +0200

    disable typecheck build in vercel

--- next.config.mjs
+++ next.config.mjs
@@ -20,5 +20,11 @@ const config = {
     locales: ['en'],
     defaultLocale: 'en',
   },
+
+  typescript: { ignoreBuildErrors: true },
+
+  eslint: { ignoreDuringBuilds: true },
+
+  swcMinify: true,
 }
 export default config
