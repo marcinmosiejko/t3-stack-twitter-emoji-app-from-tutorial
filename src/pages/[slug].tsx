ad56b9fc872fca4e3e5787264126ba65f4c71e9c
Marcin Mosiejko <marcin@stratokit.io>
2023-05-08 12:29:52 +0200

    ssg helper, single post page

--- src/pages/[slug].tsx
+++ src/pages/[slug].tsx
@@ -1,7 +1,10 @@
 import { api } from '~/utils/api'
 import Head from 'next/head'
 import Image from 'next/image'
-
+import { PageLayout } from '~/components/layout'
+import { PostView } from '~/components/postview'
+import { LoadingPage } from '~/components/loading'
+import { generateSSGHelper } from '~/server/helpers/ssgHelper'
 import type { GetStaticProps, NextPage } from 'next'
 
 const ProfileFeed = (props: { userId: string }) => {
@@ -55,20 +58,8 @@ const ProfilePage: NextPage<{ username: string }> = ({ username }) => {
   )
 }
 
-import { createServerSideHelpers } from '@trpc/react-query/server'
-import { appRouter } from '~/server/api/root'
-import { prisma } from '~/server/db'
-import superjson from 'superjson'
-import { PageLayout } from '~/components/layout'
-import { PostView } from '~/components/postview'
-import { LoadingPage } from '~/components/loading'
-
 export const getStaticProps: GetStaticProps = async (context) => {
-  const ssg = createServerSideHelpers({
-    router: appRouter,
-    ctx: { prisma, userId: null },
-    transformer: superjson, // optional - adds superjson serialization
-  })
+  const ssg = generateSSGHelper()
 
   const slug = context.params?.slug
 
--- src/pages/post/[id].tsx
+++ src/pages/post/[id].tsx
@@ -1,36 +1,48 @@
 import { api } from '~/utils/api'
 import Head from 'next/head'
-import Image from 'next/image'
-import { useUser } from '@clerk/nextjs'
+import { PageLayout } from '~/components/layout'
+import { PostView } from '~/components/postview'
+import { generateSSGHelper } from '~/server/helpers/ssgHelper'
+import type { GetStaticProps, NextPage } from 'next'
 
-import dayjs from 'dayjs'
-import relativeTime from 'dayjs/plugin/relativeTime'
+const SinglePostPage: NextPage<{ id: string }> = ({ id }) => {
+  const { data } = api.posts.getById.useQuery({
+    id,
+  })
 
-import { type NextPage } from 'next'
-import { type RouterOutputs } from '~/utils/api'
-import { LoadingPage, LoadingSpinner } from '~/components/loading'
-import { useState } from 'react'
-import toast from 'react-hot-toast'
-
-dayjs.extend(relativeTime)
-
-const SinglePostPage: NextPage = () => {
-  const { isLoaded: userLoaded, isSignedIn } = useUser()
-  // Start fetching asap so it's ready (stored in cache) once the user is loaded
-  api.posts.getAll.useQuery()
-
-  // return empty div if user isn't loaded yet (it happens fast so no point to show spinner)
-  if (!userLoaded) return <div />
+  if (!data) return <div>404</div>
 
   return (
     <>
       <Head>
-        <title>Profile</title>
-        <link rel="icon" href="/favicon.ico" />
+        <title>{`${data.post.content} - ${data.author.username}`}</title>
       </Head>
-      <main className="flex h-screen justify-center">Post View</main>
+      <PageLayout>
+        <PostView {...data} />
+      </PageLayout>
     </>
   )
+}
+
+export const getStaticProps: GetStaticProps = async (context) => {
+  const ssg = generateSSGHelper()
+
+  const id = context.params?.id
+
+  if (typeof id !== 'string') throw new Error('no id') // proper solution would be to return a different page
+
+  await ssg.posts.getById.prefetch({ id })
+
+  return {
+    props: {
+      trpcState: ssg.dehydrate(), // TL;DR: data is there when the page loads, so it's loading state will never be hit; takes all the  things we fetched and puts in a shape that can be parsed through nextjs serverside props (static props) and then on the app side we will hydrate all that data through react query into the page
+      id,
+    },
+  }
+}
+
+export const getStaticPaths = () => {
+  return { paths: [], fallback: 'blocking' }
 }
 
 export default SinglePostPage
